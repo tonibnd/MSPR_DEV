b48710ef28db45ca24c12eb53b3fe6b02c0314fe
Maxence <maxence.poizat@etu.uca.fr>
2024-02-21 19:37:26 +0100

    Installation automatique des packages + Mise en page commentaires

--- Seahawks_Harvester/SeahawksHavester.py
+++ Seahawks_Harvester/SeahawksHavester.py
@@ -1,20 +1,57 @@
+import subprocess
+import pkg_resources
+import sys
+
+# Liste des dépendances requises par l'application.
+required_packages = ['requests', 'scapy', 'python-nmap']
+
+def install_packages(packages):
+    """
+    Installe les paquets spécifiés qui ne sont pas déjà installés.
+
+    Parameters:
+    - packages (list): Une liste de noms de paquets à installer.
+
+    Cette fonction utilise le module pip pour installer les paquets spécifiés.
+    Elle vérifie d'abord si chaque paquet est déjà installé, puis installe les paquets manquants.
+    """
+
+    installed_packages = {pkg.key for pkg in pkg_resources.working_set}
+    missing_packages = [pkg for pkg in packages if pkg not in installed_packages]
+    if missing_packages:
+        print("Installation des paquets manquants : " + ", ".join(missing_packages))
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing_packages])
+
+# Assurez-vous que toutes les dépendances sont installées au démarrage.
+install_packages(required_packages)
+
+# Importations après l'installation des paquets pour éviter des erreurs d'importation.
 import tkinter as tk
 from tkinter import ttk, scrolledtext
 import nmap
 import concurrent.futures
-import time
 import socket
 from scapy.all import ARP, Ether, srp
 import requests
 import threading
 import os
 import re
-import sys
-import subprocess
+import platform
+import time
 import platform
 
-# Fonction pour envoyer des données à un serveur
 def send_data_to_server(url, data):
+    """
+    Envoie des données à un serveur via une requête POST.
+
+    Parameters:
+    - url (str): L'URL du serveur auquel envoyer les données.
+    - data (dict): Le dictionnaire contenant les données à envoyer.
+
+    Cette fonction tente d'envoyer des données à un serveur spécifié par URL.
+    Elle imprime le statut de la requête. En cas de succès, un message de confirmation est affiché.
+    En cas d'échec, le code de statut de la réponse est affiché.
+    """
     try:
         response = requests.post(url, json=data)
         print(data)
@@ -25,8 +62,20 @@ def send_data_to_server(url, data):
     except Exception as e:
         print(f"Error sending data to server: {e}")
 
-# Obtenir la plage d'adresses IP locales
+
 def get_local_ip_range():
+    """
+    Détermine la plage d'adresses IP locales de la machine en se basant sur son adresse IP actuelle.
+
+    Cette fonction crée une socket UDP temporaire pour se connecter à un serveur DNS public (ici, 8.8.8.8 sur le port 80),
+    afin de déterminer l'adresse IP actuelle de la machine sur le réseau. Elle ne transmet pas de données.
+    Ensuite, elle extrait le préfixe de l'adresse IP et construit une plage d'adresses IP au format CIDR (Classless Inter-Domain Routing),
+    supposant un masque de sous-réseau de 24 bits (255.255.255.0).
+
+    Returns:
+    str: La plage d'adresses IP locales au format CIDR.
+    """
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     local_ip_address = s.getsockname()[0]
@@ -35,14 +84,40 @@ def get_local_ip_range():
     ip_range = f"{ip_prefix}/24"
     return ip_range
 
-# Fonction pour effectuer un balayage ping
+
 def ping_sweep(ip_range):
+    """
+    Exécute un balayage de ping sur une plage d'adresses IP pour déterminer les hôtes actifs.
+
+    Parameters:
+    - ip_range (str): La plage d'adresses IP à balayer.
+
+    Returns:
+    list: Une liste d'adresses IP qui ont répondu au ping.
+    """
+
     arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_range)
     answered, _ = srp(arp_request, timeout=2, verbose=False)
     return [received_packet[1].psrc for received_packet in answered]
 
-# Fonction pour scanner avec Nmap
 def nmapscan(target, total_targets, scanned_count, results_container, progress_var, percentage_label, root):
+    """
+    Exécute un scan Nmap sur une adresse IP cible.
+
+    Parameters:
+    - target (str): L'adresse IP cible à scanner.
+    - total_targets (int): Le nombre total d'adresses IP à scanner.
+    - scanned_count (list): Une liste contenant un seul élément, le nombre d'adresses IP déjà scannées.
+    - results_container (list): Une liste pour stocker les résultats du scan.
+    - progress_var (tkinter.DoubleVar): Une variable de progression pour mettre à jour l'interface utilisateur.
+    - percentage_label (tkinter.Label): Un label pour afficher le pourcentage de progression.
+    - root (tkinter.Tk): La fenêtre principale de l'application.
+
+    Cette fonction utilise le module python-nmap pour exécuter un scan Nmap sur une adresse IP cible.
+    Elle met à jour l'interface utilisateur avec le pourcentage de progression à l'aide de la variable de progression et du label.
+    Elle stocke les résultats du scan dans une liste pour les envoyer au serveur ultérieurement.
+    """
+
     nm = nmap.PortScanner()
     nm.scan(target, arguments='-A')
 
@@ -77,14 +152,29 @@ def nmapscan(target, total_targets, scanned_count, results_container, progress_v
     progress_percentage = (scanned_count[0] / total_targets) * 100
     progress_var.set(progress_percentage)
     percentage_label.config(text=f"{int(progress_percentage)}%")
-    root.update_idletasks()  # Mettre à jour l'interface utilisateur avec le pourcentage de progression
+    # Mettre à jour l'interface utilisateur avec le pourcentage de progression
+    root.update_idletasks() 
 
     send_data_to_server("http://192.168.206.143:5000/api/data", results)
 
     results_container.extend(results)
 
-# Fonction principale pour démarrer le scan
 def start_scan(progress_var, percentage_label, text_widget, root):
+    """
+    Démarre un scan de réseau en utilisant un balayage de ping pour déterminer les hôtes actifs,
+    puis un scan Nmap pour obtenir des informations détaillées sur chaque hôte actif.
+
+    Parameters:
+    - progress_var (tkinter.DoubleVar): Une variable de progression pour mettre à jour l'interface utilisateur.
+    - percentage_label (tkinter.Label): Un label pour afficher le pourcentage de progression.
+    - text_widget (tkinter.scrolledtext.ScrolledText): Un widget de texte pour afficher les résultats du scan.
+    - root (tkinter.Tk): La fenêtre principale de l'application.
+
+    Cette fonction utilise le module concurrent.futures pour exécuter plusieurs scans Nmap en parallèle.
+    Elle met à jour l'interface utilisateur avec le pourcentage de progression à l'aide de la variable de progression et du label.
+    Elle affiche les résultats du scan dans un widget de texte.
+    """
+
     text_widget.configure(state='normal')
     text_widget.delete(1.0, tk.END)
     text_widget.configure(state='disabled')
@@ -124,14 +214,30 @@ def start_scan(progress_var, percentage_label, text_widget, root):
             text_widget.insert(tk.END, "No open ports found.\n")
     text_widget.configure(state='disabled')
 
-# Ajoutons une fonction pour obtenir le nom de l'hôte et l'adresse IP locale
 def get_host_info():
+    """
+    Obtient le nom de l'hôte et l'adresse IP locale de la machine.
+
+    Returns:
+    tuple: Un tuple contenant le nom de l'hôte et l'adresse IP locale.
+    """
+
     host_name = socket.gethostname()
     local_ip = socket.gethostbyname(host_name)
     return host_name, local_ip
 
-# Ajoutons une fonction pour calculer la latence moyenne d'accès WAN
 def get_wan_latency(target='8.8.8.8', count=4):
+    """
+    Calcule la latence moyenne d'accès WAN en utilisant la commande ping.
+
+    Parameters:
+    - target (str): L'adresse IP ou le nom d'hôte à pinguer.
+    - count (int): Le nombre de paquets à envoyer.
+
+    Returns:
+    str: La latence moyenne en millisecondes, ou "N/A" si la commande ping a échoué.
+    """
+
     # Déterminer le système d'exploitation
     oper = platform.system()
 
@@ -142,18 +248,14 @@ def get_wan_latency(target='8.8.8.8', count=4):
         ping_cmd = f"ping -c {count} {target}"
 
     try:
-        # Exécuter la commande ping
         response = subprocess.check_output(ping_cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
 
-        # Rechercher la latence moyenne dans la réponse
+        # Rechercher la latence moyenne dans la réponse en fonction de l'OS
         if oper == "Windows":
-            # Le format de sortie sous Windows inclut une moyenne après 'Moyenne = '
             match = re.search(r'Moyenne = (\d+)ms', response)
         else:
-            # Le format de sortie sous Unix/Linux inclut une moyenne après '/avg/'
             match = re.search(r'/(\d+\.\d+)/', response)
 
-        # Retourner la latence moyenne si disponible
         if match:
             return match.group(1)
     except subprocess.CalledProcessError as e:
@@ -162,13 +264,18 @@ def get_wan_latency(target='8.8.8.8', count=4):
     return "N/A"
 
 def update_application():
+    """
+    Met à jour l'application en récupérant les dernières modifications à partir du dépôt GitLab.
+
+    Cette fonction utilise git fetch pour vérifier les mises à jour, puis git pull pour appliquer les modifications.
+    Si des mises à jour sont disponibles, l'application est redémarrée pour les appliquer.
+    """
+
     try:
-        # URL de votre dépôt GitLab
         repo_url = 'https://github.com/tonibnd/MSPR_DEV'
-        # Chemin local de votre application
+        
         local_repo_path = "D:\\cours\\EPSI\\MSPRs\\MSPR_DEV"
 
-        # Vérifie que le dépôt local est configuré pour utiliser le bon remote URL
         current_repo_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url'], cwd=local_repo_path).decode('utf-8').strip()
         if current_repo_url != repo_url:
             print(f"Le dépôt local est configuré pour utiliser {current_repo_url}, qui est différent de {repo_url}")
@@ -194,8 +301,16 @@ def update_application():
         print(f"An error occurred while updating the application: {e}")
 
 
-# Interface graphique
 def create_gui():
+    """
+    Crée l'interface graphique de l'application.
+
+    Cette fonction utilise le module tkinter pour créer une interface utilisateur simple.
+    Elle crée une fenêtre principale avec des widgets pour afficher les informations sur l'hôte, la latence WAN,
+    un bouton pour mettre à jour l'application, un bouton pour démarrer le scan, une barre de progression, un label de pourcentage,
+    un widget de texte pour afficher les résultats du scan, et un label de version.
+    """
+
     root = tk.Tk()
     root.title("Network Scanner")
     
